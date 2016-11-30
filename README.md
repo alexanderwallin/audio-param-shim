8d020d0800aba6cc3d514f3b1cb2e4eaf911045a
Alexander Wallin <office@alexanderwallin.com>
2016-11-30 20:02:19 +0000

    I want to shimmy-shimmy-shimmy / through the break of dawn yeah

---
+++ README.md
@@ -0,0 +1,113 @@
+# Shimmed `AudioParam`s
+
+`audio-param-shim` lets you create objects that work aslmost exactly like [`AudioParam`s](https://developer.mozilla.org/en-US/docs/Web/API/AudioParam), except it doesn't actually touch any web audio stuff. Instead, one can subscribe for value changes and then do whatever one wants with that value.
+
+All value scheduling methods are inherited from [`pseudo-audio-param`](https://github.com/mohayonao/pseudo-audio-param) by [@mohayonao](https://github.com/mohayonao).
+
+### Features
+
+* Create `AudioParam` shims with custom names and default, min and max values
+* Scheduling values as you would on an `AudioParam`, such as with `exponentialRampToValueAtTime(value, time)`
+* Subscribe to (and unsubscribe from) value changes so that you can act upon them
+
+### Missing features
+
+* Using `AudioNode`s to modulate the parameter value
+
+### Use case
+
+I'm working on an audio library that does it's own version of a built-in Web Audio API functionality. I do, however, want it to be completely interchangable. This means exposing an `AudioParam` API to the developer, hence this library.
+
+### When not to use
+
+If you want an extension that works directly on `AudioNode`s.
+
+
+## Installation
+
+```sh
+npm i -S custom-audio-param
+```
+
+
+## Usage
+
+```js
+import createAudioParam from 'custom-audio-param'
+
+// Create a NoiseParam class
+const NoiseParam = createAudioParam('wobble', 0.25, 0, 1)
+
+// Create a param instance and change its value
+const ctx = new AudioContext()
+const noise = new NoiseParam(ctx)
+
+noise.value = 0.4 // -> 0.4
+noise.value = 4   // -> 1
+noise.value = -1  // -> 0
+
+// Change the value exponentially over 3 seconds and change the
+// amount of noise accordingly
+let noiseAmplitude = noise.value
+// Use noiseAmplitude in a ScriptProcessorNode or something...
+
+noise.subscribe(newValue => noiseAmplitude = newValue)
+noise.exponentialRampToValueAtTime(0.9, ctx.currentTime + 3)
+```
+
+
+## API
+
+### Top-level functions
+
+##### `createAudioParam(name, defaultValue, minValue, maxValue)`
+
+Returns a `AudioParamShim` class that looks and acts just like an `AudioParam` instance, except it doesn't actually touch any web audio stuff. Instead, one can subscribe for value changes and then do whatever one wants with that value.
+
+| Argument       | Type    | Description                   |
+------------------------------------------------------------
+| `name`         | String  | The parameter's name          |
+| `defaultValue` | Number  | The parameter's default value |
+| `minValue`     | Number  | The parameter's minimum value |
+| `maxValue`     | Number  | The parameter's maximum value |
+
+### `AudioParamShim` API
+
+#### Properties
+
+| Property                  | Type   | Description                   |
+----------------------------------------------------------------------
+| `defaultValue` (readonly) | Number | The parameter's default value |
+| `maxValue` (readonly)     | Number | The parameter's max value     |
+| `minValue` (readonly)     | Number | The parameter's min value     |
+| `name` (readonly)         | Number | The parameter's name          |
+
+#### Methods
+
+##### `subscribe(fn)`
+
+Subscribes to value changes.
+
+| Argument       | Type      | Description                   |
+--------------------------------------------------------------
+| `fn`           | Function  | A callback function           |
+
+##### `unsubscribe(fn)`
+
+Unsubscribes from value changes.
+
+| Argument       | Type      | Description                   |
+--------------------------------------------------------------
+| `fn`           | Function  | A callback function           |
+
+
+## Credits
+
+* [@mohayonao](https://github.com/mohayonao) for creating [`pseudo-audio-param`](https://github.com/mohayonao/pseudo-audio-param)
+
+
+## See also
+
+* [mohayonao/pseudo-audio-param](https://github.com/mohayonao/pseudo-audio-param)
+* [mohayonao/audio-param-transform](https://github.com/mohayonao/audio-param-transform)
+* [mmckegg/custom-audio-node](https://github.com/mmckegg/custom-audio-node)
---
+++ src/createAudioParam.js
@@ -0,0 +1,149 @@
+import PseudoAudioParam from 'pseudo-audio-param'
+import invariant from 'invariant'
+import isNumber from 'lodash.isnumber'
+import { readonly, nonenumerable, nonconfigurable } from 'core-decorators'
+
+const AudioContextConstructor = window.AudioContext || window.webkitAudioContext
+
+/**
+ * Returns a constructor/class/function that looks and acts just like an
+ * `AudioParam` instance, except it doesn't actually touch any web audio
+ * stuff. Instead, one can subscribe for value changes and then do whatever
+ * one wants with that value.
+ *
+ * @param  {String} name          The parameter's name
+ * @param  {Number} defaultValue  The parameter's default value
+ * @param  {Number} minValue      The parameter's minimum value
+ * @param  {Number} maxValue      The parameter's maximum value
+ * @return {Function}             A AudioParamShim class
+ */
+export default function createAudioParam(name, defaultValue, minValue, maxValue) {
+
+  // Make sure we have what we need
+  invariant(typeof name === 'string' && name.trim().length > 0, 'You must provide a non-empty parameter name to createAudioParam(name, defaultValue, minValue, maxValue)')
+  invariant(isNumber(defaultValue), 'You must provide a numeric default value to createAudioParam(name, defaultValue, minValue, maxValue)')
+  invariant(isNumber(minValue), 'You must provide a numeric min value to createAudioParam(name, defaultValue, minValue, maxValue)')
+  invariant(isNumber(maxValue), 'You must provide a numeric max value to createAudioParam(name, defaultValue, minValue, maxValue)')
+  invariant(defaultValue >= minValue, `The default value ${defaultValue} cannot be lesser than the minimum value ${minValue}`)
+  invariant(defaultValue <= maxValue, `The default value ${defaultValue} cannot be greater than the maximum value ${maxValue}`)
+
+  /**
+   * A bespoke, hand-crafted audio param shim class having the
+   * same API as real `AudioParam`.
+   */
+  class AudioParamShim extends PseudoAudioParam {
+
+    /**
+     * Constructor
+     *
+     * @param  {AudioContext} context  An AudioContext instance
+     * @return {Function}              A custom audio param constructor
+     * @throws {TypeError}             If context is not an instance if `(webkit)AudioContext`
+     */
+    constructor(context) {
+      super()
+
+      if (context instanceof AudioContextConstructor === false) {
+        throw new TypeError(`The provided context is not an AudioContext or webkitAudioContext`)
+      }
+
+      this.context = context
+      this.value = this.defaultValue
+
+      const update = () => {
+        const currentValue = this.value
+        this._subscribers.forEach(fn => fn(currentValue))
+        window.requestAnimationFrame(update)
+      }
+
+      update()
+    }
+
+    /**
+     * The parameter's name
+     */
+    @nonenumerable
+    @nonconfigurable
+    @readonly
+    name = name.trim()
+
+    /**
+     * An array of callback functions that gets notify of value changes
+     */
+    @nonenumerable
+    _subscribers = []
+
+    /**
+     * Returns the parameter's current value
+     *
+     * @return {Number} The parameter's current value
+     */
+    get value() {
+      return this.getValueAtTime(this.context.currentTime)
+    }
+
+    /**
+     * Sets the parameter's value to a new value. The new value is clamped inside
+     * to range given by `minValue` and `maxValue`.
+     *
+     * @param  {Number} value  A new value
+     */
+    set value(value) {
+      let clampedValue = value
+
+      if (value > this.maxValue) {
+        console.warn(`The value ${value} is greater than the max value ${this.maxValue}`)
+        clampedValue = this.maxValue
+      }
+      if (value < this.minValue) {
+        console.warn(`The value ${value} is lesser than the min value ${this.minValue}`)
+        clampedValue = this.minValue
+      }
+
+      this.setValueAtTime(clampedValue, this.context.currentTime)
+    }
+
+    /**
+     * The parameter's default value
+     */
+    @readonly
+    @nonconfigurable
+    defaultValue = defaultValue
+
+    /**
+     * The parameter's minimum value
+     */
+    @readonly
+    @nonconfigurable
+    minValue = minValue
+
+    /**
+     * The parameter's maximum value
+     */
+    @readonly
+    @nonconfigurable
+    maxValue = maxValue
+
+    /**
+     * Adds a functions to the list of callback functions that are invoked
+     * with the parameter's value whenever it changes.
+     *
+     * @param  {Function} fn  A callback function
+     */
+    subscribe(fn) {
+      this._subscribers = [...this._subscribers, fn]
+    }
+
+    /**
+     * Removes a function from the list of callback functions that are invoked
+     * with the parameter's value whenever it changes.
+     *
+     * @param  {Function} fn  The callback function to remove
+     */
+    unsubscribe(fn) {
+      this._subscribers = this._subscribers.filter(subscriber => subscriber !== fn)
+    }
+  }
+
+  return AudioParamShim
+}
